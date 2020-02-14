df6f3f0f8ea151fb257193c81b9885891cf4d6ae
Kamil Sacek <kamil.sacek@navertica.com>
2020-02-14 07:34:43 +0100

    Add mermaid graph output, add apps w/o relation
    mermaid graph syntax could be used in Azure DevOps Wiki to display graphs. No graph optimization, thus the output is somehow worse than using graphviz, but it have click-through to the repository etc.

--- Get-ALAppGraphFromAzureDevOps.ps1
+++ Get-ALAppGraphFromAzureDevOps.ps1
@@ -6,7 +6,8 @@ param(
     [parameter(Mandatory=$true)]
     $PAT,
     $AppJsonPath='MainApp',
-    $IncludeMicrosoft
+    [switch]$Mermaid,
+    [switch]$IncludeMicrosoft
 )
 import-module vsteam
 
@@ -14,14 +15,35 @@ set-vsteamaccount -Account $Account -PersonalAccessToken $PAT
 $repos = Get-VSTeamGitRepository -ProjectName $ProjectName
 $info = Get-VSTeamInfo
 
-Write-Host "digraph G {"
-Write-Host "    rankdir=""LR"""
-foreach($repo in $repos) {
+if ($Mermaid) {
+    Write-Host "::: mermaid"
+    Write-Host "graph LR"
+} else {
+    Write-Host "digraph G {"
+    Write-Host "    rankdir=""LR"""
+}
+foreach($repo in ($repos | Sort-Object -Property name)) {
     $appJson=Invoke-VSTeamRequest -version 5.1 -method get -Url "$($info.Account)/$ProjectName/_apis/git/repositories/$($repo.Name)/items?path=$($AppJsonPath)/app.json" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-    foreach($dep in $appJson.dependencies) {
-        if ($IncludeMicrosoft -or ($dep.publisher -ne 'Microsoft')) {
-            Write-Host "    ""$($appJson.name)""->""$($dep.name)"""
+    if ($appJson) {
+        if ($Mermaid) {
+            Write-Host "$($appJson.name -replace ' ','')[$($appJson.name)];"
+            Write-Host "click $($appJson.name -replace ' ','') ""$($repo.RemoteUrl -replace '//.*@','//')"";"
+        } else {
+            Write-Host "    ""$($appJson.name)"""
+        }
+        foreach($dep in $appJson.dependencies) {
+            if ($IncludeMicrosoft -or ($dep.publisher -ne 'Microsoft')) {
+                if ($Mermaid) {
+                    Write-Host "$($appJson.name -replace ' ','')-->$($dep.name -replace ' ','');"
+                } else {
+                    Write-Host "    ""$($appJson.name)""->""$($dep.name)"""
+                }
+            }
         }
     }
 }
-Write-Host "}"
+if ($Mermaid) {
+    Write-Host ":::"
+} else {
+    Write-Host "}"
+}
