6c70b5040d8bdaee15595ff4ddbdb285c10cc548
Kamil Sacek <kamil.sacek@navertica.com>
2020-02-14 07:43:50 +0100

    Send result to output, not host
    CHanging write-host to write-output to be able to work with the result in pipeline

--- Get-ALAppGraphFromAzureDevOps.ps1
+++ Get-ALAppGraphFromAzureDevOps.ps1
@@ -16,34 +16,34 @@ $repos = Get-VSTeamGitRepository -ProjectName $ProjectName
 $info = Get-VSTeamInfo
 
 if ($Mermaid) {
-    Write-Host "::: mermaid"
-    Write-Host "graph LR"
+    Write-Output "::: mermaid"
+    Write-Output "graph LR"
 } else {
-    Write-Host "digraph G {"
-    Write-Host "    rankdir=""LR"""
+    Write-Output "digraph G {"
+    Write-Output "    rankdir=""LR"""
 }
 foreach($repo in ($repos | Sort-Object -Property name)) {
     $appJson=Invoke-VSTeamRequest -version 5.1 -method get -Url "$($info.Account)/$ProjectName/_apis/git/repositories/$($repo.Name)/items?path=$($AppJsonPath)/app.json" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
     if ($appJson) {
         if ($Mermaid) {
-            Write-Host "$($appJson.name -replace ' ','')[$($appJson.name)];"
-            Write-Host "click $($appJson.name -replace ' ','') ""$($repo.RemoteUrl -replace '//.*@','//')"";"
+            Write-Output "$($appJson.name -replace ' ','')[$($appJson.name)];"
+            Write-Output "click $($appJson.name -replace ' ','') ""$($repo.RemoteUrl -replace '//.*@','//')"";"
         } else {
-            Write-Host "    ""$($appJson.name)"""
+            Write-Output "    ""$($appJson.name)"""
         }
         foreach($dep in $appJson.dependencies) {
             if ($IncludeMicrosoft -or ($dep.publisher -ne 'Microsoft')) {
                 if ($Mermaid) {
-                    Write-Host "$($dep.name -replace ' ','')-->$($appJson.name -replace ' ','');"
+                    Write-Output "$($dep.name -replace ' ','')-->$($appJson.name -replace ' ','');"
                 } else {
-                    Write-Host "    ""$($appJson.name)""->""$($dep.name)"""
+                    Write-Output "    ""$($appJson.name)""->""$($dep.name)"""
                 }
             }
         }
     }
 }
 if ($Mermaid) {
-    Write-Host ":::"
+    Write-Output ":::"
 } else {
-    Write-Host "}"
+    Write-Output "}"
 }
